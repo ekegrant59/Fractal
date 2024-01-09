1661a2d0362bfe23786302959df050ed40902987
ekegrant59 <107748298+ekegrant59@users.noreply.github.com>
2024-01-09 23:44:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ app.use(function (req, res, next) {
 var transporter = nodemailer.createTransport(
     {
         host: process.env.EMAIL_HOST,
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
